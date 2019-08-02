cb8126893dfeb963d99303f36865bcdd876e0390
daixl <macache@163.com>
2019-08-02 15:18:59 +0800

    add Decelerate Feature
    add Decelerate Feature for AppleUIScrollView with Pop which is a fatastic lib from FaceBook

--- AppleUIScrollView/AppleUIScrollView.m
+++ AppleUIScrollView/AppleUIScrollView.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppleUIScrollView.h"
+#import <pop/POP.h>
 
 @interface AppleUIScrollView ()
 @property (nonatomic, assign) CGRect startBounds;
@@ -44,11 +45,11 @@
             CGRect bounds = self.startBounds;
 
             //change UIScrollView bounds
-            //for X-axis
-            CGFloat newBoundsOriginX = bounds.origin.x - translation.x;
-            CGFloat minBoundsOriginX = 0.0;
-            CGFloat maxBoundsOriginX = self.contentSize.width - bounds.size.width;
-            bounds.origin.x = fmax(minBoundsOriginX, fmin(newBoundsOriginX, maxBoundsOriginX));
+            //for X-axis  just scroll in vertical direction
+//            CGFloat newBoundsOriginX = bounds.origin.x - translation.x;
+//            CGFloat minBoundsOriginX = 0.0;
+//            CGFloat maxBoundsOriginX = self.contentSize.width - bounds.size.width;
+//            bounds.origin.x = fmax(minBoundsOriginX, fmin(newBoundsOriginX, maxBoundsOriginX));
 
             //for Y-axis
             CGFloat newBoundsOriginY = bounds.origin.y - translation.y;
@@ -59,7 +60,42 @@
             self.bounds = bounds;
         }
             break;
-        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateEnded: {
+            CGPoint velocity =  [panGesture velocityInView:self];
+            if (self.bounds.size.width > self.contentSize.width) {
+                velocity.x = 0;
+            }
+            if (self.bounds.size.height > self.contentSize.height) {
+                velocity.y = 0;
+            }
+            velocity.x = -velocity.x;
+            velocity.y = -velocity.y;
+            velocity.x = 0;     // just scroll in vertical direction
+
+            POPDecayAnimation *decayAnimation = [POPDecayAnimation animationWithPropertyNamed:kPOPViewBounds];
+            decayAnimation.clampMode = kPOPAnimationClampBoth;
+            POPAnimatableProperty *prop = [POPAnimatableProperty propertyWithName:@"com.rounak.boundsY" initializer:^(POPMutableAnimatableProperty *prop) {
+                // read value
+                prop.readBlock = ^(id obj, CGFloat values[]) {
+                    NSLog(@"readBlock values: %f", values[0]);
+                    values[0] = [obj bounds].origin.x;
+                    values[1] = [obj bounds].origin.y;
+                };
+                // write value
+                prop.writeBlock = ^(id obj, const CGFloat values[]) {
+                    CGRect tempBounds = [obj bounds];
+                    NSLog(@"writeBlock values: %f", values[0]);
+                    tempBounds.origin.x = values[0];
+                    tempBounds.origin.y = values[1];
+                    [obj setBounds:tempBounds];
+                };
+                // dynamics threshold
+                prop.threshold = 0.01;
+            }];
+            decayAnimation.property = prop;
+            decayAnimation.velocity = [NSValue valueWithCGPoint:velocity];
+            [self pop_addAnimation:decayAnimation forKey:@"decelerate"];
+        }
             break;
         default:
             break;
